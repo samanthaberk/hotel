204f8fbba4dc8de01cd159eca99d621488937906
Samantha Berk <sberk10@gmail.com>
2018-03-05 13:50:25 -0800

    Wrote tests for booking_manager constructor to instantiate w/ set num of rooms, empty reservations array, and standard price

--- specs/booking_manager_spec.rb
+++ specs/booking_manager_spec.rb
@@ -7,7 +7,32 @@ describe "Booking Manager Class" do
       booking_manager = Hotel::BookingManager.new
       booking_manager.must_be_kind_of Hotel::BookingManager
     end
-    
+
+    it "establishes the base data structures when instantiated" do
+      booking_manager = Hotel::BookingManager.new
+      [:rooms, :price, :reservations].each do |data|
+        booking_manager.must_respond_to data
+      end
+
+      booking_manager.rooms.must_be_kind_of Array
+      booking_manager.rooms.each do |room|
+        room.must_be_kind_of Integer
+      end
+
+      booking_manager.price.must_be_kind_of Float
+      booking_manager.price.must_equal 200.00
+
+      booking_manager.reservations.must_be_kind_of Array
+      booking_manager.reservations.must_equal []
+    end
+
+    # pseudocode starts here
+      # input: none
+      # output: list of 20 hotel rooms
+      #         fixed price of hotel rooms
+      #         empty array to store reservations
+
+
   end
 
 end

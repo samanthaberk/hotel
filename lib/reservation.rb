049f8d15f94f7e629085332950649182590924ef
Samantha Berk <sberk10@gmail.com>
2018-03-05 15:50:33 -0800

    Wrote code to ensure valid date entry for reservation class constructor

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -6,6 +6,14 @@ module Hotel
     attr_accessor :check_in, :check_out
 
     def initialize(check_in, check_out)
+      if check_out < check_in
+        raise ArgumentError.new("Check-out date cannot be earlier than check-in.")
+      end
+
+      if check_out == check_in
+        raise ArgumentError.new("Check-in and check-out dates cannot be the same.")
+      end
+
       @check_in = Date.parse(check_in)
       @check_out = Date.parse(check_out)
     end # constructor

a492767dd39d5b35e3866fbefbc81c0ea6400d44
Samantha Berk <sberk10@gmail.com>
2018-03-05 16:17:46 -0800

    Changed input for reservation constructor to a hash that includes a reservation id, room number, and check-in/check-out dates

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,21 +1,26 @@
 require 'date'
+require_relative 'booking_manager'
 
 module Hotel
 
   class Reservation
-    attr_accessor :check_in, :check_out
+    attr_accessor :id, :room_num, :check_in, :check_out
 
-    def initialize(check_in, check_out)
-      if check_out < check_in
+    def initialize(reservation_data)
+      @id = reservation_data[:id]
+      @room_num = reservation_data[:room_num]
+      @check_in = Date.parse(reservation_data[:check_in])
+      @check_out = Date.parse(reservation_data[:check_out])
+
+      # Check for validity of room number and dates
+      if @id > 20 || @id < 1
+        raise ArgumentError.new("Invalid room number.")
+      elsif @check_out < @check_in
         raise ArgumentError.new("Check-out date cannot be earlier than check-in.")
-      end
-
-      if check_out == check_in
+      elsif @check_out == @check_in
         raise ArgumentError.new("Check-in and check-out dates cannot be the same.")
       end
 
-      @check_in = Date.parse(check_in)
-      @check_out = Date.parse(check_out)
     end # constructor
 
   end # reservation class
--- specs/reservation_spec.rb
+++ specs/reservation_spec.rb
@@ -5,18 +5,22 @@ describe "Reservation class" do
   describe "Reservation instantiation" do
 
     it "is an instance of Reservation" do
-      new_reservation = Hotel::Reservation.new('15-03-2018', '17-03-2018')
+      new_reservation = Hotel::Reservation.new({id: 1, room_num: 1, check_in: '15-03-2018', check_out: '17-03-2018'})
       new_reservation.must_be_kind_of Hotel::Reservation
     end
 
+    it "raises and exception if the room number does not exist" do
+      proc{ Hotel::Reservation.new({id: 1, room_num: 34, check_in: '15-03-2018', check_out: '17-03-2018'})}
+    end
+
     it "raises an exception if the check-in date is after the check-out date" do
-      proc{ Hotel::Reservation.new('17-03-2018', '15-03-2018')}.must_raise ArgumentError
+      proc{ Hotel::Reservation.new({id: 1, room_num: 2, check_in: '17-03-2018', check_out: '15-03-2018'})}.must_raise ArgumentError
     end
 
     it "raises an exception if the check-in and check_out dates are the same" do
-      proc{ Hotel::Reservation.new('17-03-2018', '17-03-2018')}.must_raise ArgumentError
+      proc{ Hotel::Reservation.new({id: 1, room_num: 2, check_in: '17-03-2018', check_out: '17-03-2018'})}.must_raise ArgumentError
     end
 
-  end
+  end # constructor
 
 end

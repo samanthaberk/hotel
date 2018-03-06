09e003b096a6007466dd882ace17dfb1ccc9a25f
Samantha Berk <sberk10@gmail.com>
2018-03-05 18:31:56 -0800

    Added tests and code to return the correct reservation instances when provided with dates as parameters

--- lib/booking_manager.rb
+++ lib/booking_manager.rb
@@ -1,4 +1,5 @@
 require 'date'
+require 'pry'
 
 module Hotel
 
@@ -15,10 +16,6 @@ module Hotel
       return @rooms
     end
 
-    def display_reservations_list
-      return @reservations
-    end
-
     def reserve_room(check_in, check_out)
       # load reservation data
       reservation_data = {
@@ -28,10 +25,21 @@ module Hotel
         check_out: check_out
       }
 
-      new_reservation = Hotel::Reservation.new(reservation_data)
+      new_reservation = Reservation.new(reservation_data)
       @reservations.push(new_reservation)
 
       return new_reservation
+    end
+
+    def display_reservations(check_in, check_out)
+      matching_reservations = []
+      @reservations.each do |reservation|
+        if reservation.check_in == Date.parse(check_in) && reservation.check_out == Date.parse(check_out)
+          matching_reservations << reservation
+        end
+      end
+      return matching_reservations
+
     end
 
   end # booking manager class
